25cf686d01a8be1e956bdda6deb12114650db295
mcleanwallen <all17053@byui.edu>
2021-10-12 17:52:13 -0600

    list

--- lesson4/js/preston-4.js
+++ lesson4/js/preston-4.js
@@ -3,6 +3,17 @@ document.getElementById('currentYear').innerHTML = d.getFullYear();
 
 document.getElementById('lastModified').innerHTML = document.lastModified
 
+
+//Responsive Menu
+
+const hambutton = document.querySelector('.hamButton');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 1050) mainnav.classList.remove('responsive')};
+
 function toggleMenu() {
     document.getElementsByClassName("navigation").classList.toggle("responsive")
 }
