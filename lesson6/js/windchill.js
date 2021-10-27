41561e656a8653d5ff22ac73932e393ccc01bb31
mcleanwallen <all17053@byui.edu>
2021-10-26 18:44:01 -0600

    trunc

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,9 +1,9 @@
-var temp = document.getElementById("normalTemp").value;
-var wind = document.getElementById("wind").value;
+var temp = document.getElementById("normalTemp").innerHTML;
+var wind = document.getElementById("wind").innerHTML;
 
 var xTemp = parseFloat(temp);
 var xWind = parseFloat(wind);
 
-var windchill = 35.74 + (.6215 * xTemp) - (35.75 * (xWind ^ .16)) + (.4275 * xTemp * (xWind ^ .16));
+let windchill = 35.74 + (.6215 * xTemp) - (35.75 * Math.pow(xWind,.16)) + (.4275 * xTemp * Math.pow(xWind,.16));
 
-document.getElementById("windchillTemp").innerHTML = windchill;
+document.getElementById("windchillTemp").innerHTML = Math.trunc(windchill);
