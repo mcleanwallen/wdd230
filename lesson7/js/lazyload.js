35b49f6e50615e72b8e7c0c52a5c8379b02d366a
mcleanwallen <all17053@byui.edu>
2021-11-04 07:43:52 -0600

    Table

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,29 +1,59 @@
-const images = document.querySelectorAll("[data-src]");
-
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if (!src) {
-        return;
-    }
-
-    img.src = src;
-}
-
-const imgOptions = {
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
 };
 
-const imgObserver = new IntersectionObserver((entries,
-    imgObserver) => {
-        entries.forEach(entry => {
-            if (!entry.isIntersecting) {
-                return;
-            } else {
-                preloadImage(entry.target);
-                imgObserver.unobserve(entry.target);
-            }
-        })
-    }, imgOptions);
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+});
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+}
   
-images.forEach(image => {
-    imgObserver.observe(image);
-})
+// const images = document.querySelectorAll("[data-src]");
+
+// const preloadImage = (img) => {
+//     img.setAttribute("src", img.getAttribute("data-src"));
+//     img.onload = () => {
+//         img.removeAttribute("data-src");
+//     };
+// };
+
+// const imgOptions = {
+//     rootMargin: '0px 0px 50px 0px',
+//     threshold: 1
+// };
+
+// const imgObserver = new IntersectionObserver((entries,
+//     imgObserver) => {
+//         entries.forEach(entry => {
+//             if (!entry.isIntersecting) {
+//                 return;
+//             } else {
+//                 preloadImage(entry.target);
+//                 imgObserver.unobserve(entry.target);
+//             }
+//         })
+//     }, imgOptions);
+  
+// images.forEach(image => {
+//     imgObserver.observe(image);
+// })
