97025fbb5b97dbc76fd52d65db2d9e62d4069314
mcleanwallen <all17053@byui.edu>
2021-11-04 20:22:07 -0600

    fixed week 6 JS

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -6,4 +6,8 @@ var xWind = parseFloat(wind);
 
 let windchill = 35.74 + (.6215 * xTemp) - (35.75 * Math.pow(xWind,.16)) + (.4275 * xTemp * Math.pow(xWind,.16));
 
-document.getElementById("windchillTemp").innerHTML = Math.trunc(windchill);
+if ((xTemp <= 50) && (xWind > 3)) {
+    document.getElementById("windchillTemp").innerHTML = Math.trunc(windchill);
+} else {
+    document.getElementById("windchillTemp").innerHTML = "N/A";
+}
