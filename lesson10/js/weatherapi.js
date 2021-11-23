dcd3adec18e669fe32107dee2f82c25d0e08b2a6
mcleanwallen <all17053@byui.edu>
2021-11-22 22:03:40 -0700

    5-day forecast

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -16,10 +16,5 @@ fetch(apiURL)
     {
       document.getElementById('windchillTemp').textContent = jsObject.main.feels_like;
     }
-    const imagesrc =
-      "https://openweathermap.org/img/w/" + jsObject.weather[0].icon + ".png"; // note the concatenation
     const desc = jsObject.weather[0].description; // note how we reference the weather array
-    document.getElementById("imagesrc").textContent = imagesrc; // informational specification only
-    document.getElementById("icon").setAttribute("src", imagesrc); // focus on the setAttribute() method
-    document.getElementById("icon").setAttribute("alt", desc);
   });
--- lesson10/js/weatherapi2.js
+++ lesson10/js/weatherapi2.js
@@ -1,5 +1,3 @@
-const weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-const d = new Date;
 const requestURL =
   "https://api.openweathermap.org/data/2.5/onecall?lat=42.09&lon=-111.87&units=imperial&exclude=current,minutely,hourly,alerts&appid=ae5d16101acf3ce3c331c608b6416c84";
 fetch(requestURL)
@@ -8,15 +6,27 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject); // temporary checking for valid response and data parsing
-    for (let i = 1; i < 7; i++ ) {
+    const weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const d = new Date;
+    for (let i = 1; i < 6; i++ ) {
         let card = document.createElement('div');
         let h4 = document.createElement('h4');
-        let h5 = document.createElement('h5');
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        let img = document.createElement('img');
         card.setAttribute("class","day");
-        h2.textContent = weekdays[d.getDate()];
-        h3.textContent = jsonObject.daily[i].temp.day;
+        h4.textContent = weekdays[(d.getDate()+i)%7];
+        p1.textContent = "High: " + jsonObject.daily[i].temp.max +"\u00B0 F";
+        p2.textContent = "Low: " + jsonObject.daily[i].temp.min +"\u00B0 F";
+        img.setAttribute("src",
+        "https://openweathermap.org/img/w/" + 
+        jsonObject.daily[i].weather[0].icon + 
+        ".png"); // note the concatenation
+        img.setAttribute("alt", jsonObject.daily[i].weather[0].description);
         card.appendChild(h4);
-        card.appendChild(h5);
+        card.appendChild(p1);
+        card.appendChild(p2);
+        card.appendChild(img);
         document.querySelector('section.fiveDay').appendChild(card);
     };
   });
